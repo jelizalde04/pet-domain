9aeed0bc43281c26f75adca285dafe30a6a16f00
Johana Elizalde <134255446+jelizalde04@users.noreply.github.com>
2025-06-03 12:00:44 -0500

    chore(models): 🐕 define pet model schema

---
+++ get-pet-by-id/models/Pet.js
@@ -0,0 +1,50 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const { v4: uuidv4 } = require("uuid");
+const sequelize = require("../config/db");
+
+const Pet = sequelize.define("Pet", {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: uuidv4,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  species: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  breed: {
+    type: DataTypes.STRING,
+  },
+  image: {
+    type: DataTypes.STRING,
+  },
+  birthdate: {
+    type: DataTypes.DATE,
+  },
+  residence: {
+    type: DataTypes.STRING,
+  },
+  gender: {
+    type: DataTypes.STRING,
+  },
+  color: {
+    type: DataTypes.STRING,
+  },
+  responsibleId: {  
+    type: DataTypes.UUID,
+    allowNull: false,
+    references: {
+      model: "Responsibles",  
+      key: "id",
+    },
+  },
+});
+
+// Relación uno a muchos sin necesidad de importar el modelo Responsible
+Pet.belongsTo(Pet, { foreignKey: "responsibleId" }); // Relación entre pet y responsibleId
+
+module.exports = Pet;
