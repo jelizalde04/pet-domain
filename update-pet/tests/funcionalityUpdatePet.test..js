02d040fa5257ab3a9738281912c856ada6630eb7
Johana Elizalde <134255446+jelizalde04@users.noreply.github.com>
2025-06-23 04:55:37 -0500

    ix (tests): 🐛fix functional test error in user service

--- update-pet/tests/funcionalityUpdatePet.test..js
+++ update-pet/tests/funcionalityUpdatePet.test..js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-describe('Create Pet Microservice', () => {
+describe('Update Pet Microservice', () => {
   test('Functionality test completed successfully', () => {
     const filePath = path.join(__dirname, '../config/db.js');
           expect(fs.existsSync(filePath)).toBe(true);
